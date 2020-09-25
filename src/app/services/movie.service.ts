fd2aad46b49a7d6e75653057b1c35677b13abbb5
Lyonel Desmarattes <leodesma08@gmail.com>
2020-09-25 12:31:24 -0500

    Added movie service and refactor call to use it.

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { movieApiConstants as mapi } from '../../movie-api-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(): Observable<any> {
+    return this.http.get(`${mapi.url}${mapi.endpoints.getPopularMovies}?api_key=${mapi.apiKey}`);
+  }
+}
